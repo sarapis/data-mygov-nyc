ab20d28d19b78f6047502051b4e37e5848b20275
loveisbluenova <danielkent910@gmail.com>
2017-12-26 18:49:16 -0300

    mailchimp

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -120,6 +120,28 @@
                                                 <div class="col-sm-6">
                                                     <iframe src="https://feed.mikle.com/widget/v2/58831/" height="402px" width="100%" class="fw-iframe" scrolling="no" frameborder="0"></iframe>
                                                 </div>
+                                                <div class="col-md-6">
+                                                    <!-- Begin MailChimp Signup Form -->
+                                                    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+                                                    <style type="text/css">
+                                                     #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+                                                     /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                                                        We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                                                    </style>
+                                                    <div id="mc_embed_signup">
+                                                    <form action="https://municipalist.us14.list-manage.com/subscribe/post?u=bc85158717f189878a1a9cf42&amp;id=7ab01d02e4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                                        <div id="mc_embed_signup_scroll">
+                                                     <label for="mce-EMAIL">Get Updates in your Inbox</label>
+                                                     <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                                                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bc85158717f189878a1a9cf42_7ab01d02e4" tabindex="-1" value=""></div>
+                                                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                                        </div>
+                                                    </form>
+                                                    </div>
+
+                                                    <!--End mc_embed_signup-->
+                                                </div>
                                             </div>                            
                                         </div>
                                     </div>
